2069e0f60a6c86ba2fc58062a37988eb76ce6382
Rory <rwilson2@ltu.edu>
2021-04-26 21:11:40 -0400

    HEADS UP
    - also like most of level 1

---
+++ trigger digit/Assets/Scripts/CanvasScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasScript : MonoBehaviour
+{
+    public PlayerScript player;
+    public GunScript gun;
+    public Text restraint;
+    public Image hpbar, unacceptableViolence;
+
+    InnocentScript[] innoList;
+    int killLimit;
+
+    private void Start()
+    {
+        innoList = FindObjectsOfType<InnocentScript>();
+        killLimit = Mathf.CeilToInt(innoList.Length * 0.70f);
+        player.reputation = killLimit;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gun.restraint > 9)
+        {
+            restraint.text = gun.restraint.ToString("000");
+        } else restraint.text = gun.restraint.ToString("00");
+
+        unacceptableViolence.fillAmount = 1 - (player.reputation/(float)killLimit);
+    }
+}
