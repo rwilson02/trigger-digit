148ed10d336480681872b1dccf0d8fb57840afbf
Rory <rwilson2@ltu.edu>
2021-04-28 02:51:27 -0400

    ENDINGS
    - can now win the game
    - also progress through levels but shhhhh
    - SICK NEW SKYBOX
    -- love that skybox

---
+++ trigger digit/Assets/Scripts/CoolSpinBro.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoolSpinBro : MonoBehaviour
+{
+    public float speed = 1;
+    void Update()
+    {
+        gameObject.transform.Rotate(Vector3.up, Time.deltaTime * speed);
+    }
+}
---
+++ trigger digit/Assets/Scripts/MovinOn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovinOn : MonoBehaviour
+{
+    ChangeScene sean;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sean = FindObjectOfType<ChangeScene>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            sean.Outie();
+        }
+    }
+}
